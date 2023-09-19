f12bead73026d8b6e3f00c49ec9f011ef1ee5949
mohanmohadikar <mohanmmohadikar@gmail.com>
2023-09-19 18:42:56 +0530

    Initial commit

---
+++ src/main/java/com/mohanmmohadikar/microbloggerbackend/MicrobloggerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mohanmmohadikar.microbloggerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobloggerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobloggerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohanmmohadikar/microbloggerbackend/MicrobloggerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohanmmohadikar.microbloggerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobloggerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
