ab535c8cde1bff44e5a2081445760a6c48d5b609
mohanmohadikar <mohanmmohadikar@gmail.com>
2023-09-30 00:18:57 +0530

    implemented JwtTokenValidator class for validation of jwt.

--- src/main/java/com/mohanmmohadikar/microbloggerbackend/config/JwtTokenValidator.java
+++ src/main/java/com/mohanmmohadikar/microbloggerbackend/config/JwtTokenValidator.java
@@ -1,13 +1,21 @@
 package com.mohanmmohadikar.microbloggerbackend.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.util.List;
 
 public class JwtTokenValidator extends OncePerRequestFilter {
     @Override
@@ -20,10 +28,24 @@ public class JwtTokenValidator extends OncePerRequestFilter {
             jwt = jwt.substring(7);
 
             try {
-                SecretKey key = "".getBytes();
+                Claims claims = Jwts
+                        .parser()
+                        .setSigningKey(JwtConstant.SECRET_KEY)
+                        .parseClaimsJwt(jwt)
+                        .getBody();
+
+                String email = String.valueOf(claims.get("email"));
+                String authorities = String.valueOf(claims.get("authorities"));
+                List<GrantedAuthority> auths = AuthorityUtils
+                        .commaSeparatedStringToAuthorityList(authorities);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, auths);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             } catch (Exception e) {
-                // TODO: handle exception
+                throw new BadCredentialsException("Invalid token");
             }
+
+            filterChain.doFilter(request, response);
         }
     }
 }
