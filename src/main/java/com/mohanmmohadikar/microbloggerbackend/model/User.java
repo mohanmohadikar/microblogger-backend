7fdf18a0d658a271484708431a07e7ea09b853b0
mohanmohadikar <mohanmmohadikar@gmail.com>
2023-10-10 02:00:38 +0530

    created User model.

---
+++ src/main/java/com/mohanmmohadikar/microbloggerbackend/model/User.java
@@ -0,0 +1,49 @@
+package com.mohanmmohadikar.microbloggerbackend.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    private String fullName;
+    private String location;
+    private String website;
+    private String dob;
+    private String email;
+    private String password;
+    private String mobile;
+    private String image;
+    private String backgroundImage;
+    private String bio;
+    private boolean reqUser;
+    private boolean loginWithGoogle;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade=CascadeType.ALL)
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade=CascadeType.ALL)
+    private List<Like> likes = new ArrayList<>();
+
+    @Embedded
+    private Verification verification;
+
+    @JsonIgnore
+    @ManyToMany
+    private List<User> followers = new ArrayList<>();
+
+    @JsonIgnore
+    @ManyToMany
+    private List<User> followings = new ArrayList<>();
+
+}
